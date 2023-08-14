6d08b76d163bad3cad70b288f68cb3fe66538a5a
özer aydın <ozeraydin57@gmail.com>
2023-08-14 15:01:45 +0300

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -13,4 +13,30 @@ namespace B1Core
     {
         public static SAPbobsCOM.Company oCompany { get; set; }
     }
+
+    public static bool SetupControl(string tableName, int version) //version for setup object
+    {
+            try
+            {
+                var ver = Data.ExecuteSql($"select U_Value from [@{tableName}] where Name='Version'").Data.Fields.Item("U_Value").Value.ToString();
+                if (ver == "")
+                {
+                    Data.ExecuteSql($"insert into [@{tableName}] (Name, U_Value) values('Version', {version - 1}) ");
+                    ver = (version - 1).ToString();
+                }
+
+                if (int.Parse(ver) >= version)
+                {
+                    return true;
+                }
+
+                var versData = Data.ExecuteSql($"update [@{tableName}] set U_Value='{int.Parse(ver) + 1}' where Name='Version'");
+
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+ }
 }
